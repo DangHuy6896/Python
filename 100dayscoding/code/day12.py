6a701bf2c8c99dbfe38abb9e7a146dc3ac837dd5
danghuy6896 <cass6896@gmail.com>
2023-11-07 14:13:16 +0700

    eccc

--- 100dayscoding/code/day12.py
+++ 100dayscoding/code/day12.py
@@ -5,43 +5,37 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 def easy_mode():
-    count_a = 10
-    guess_a = True
-    while guess_a:
+    count_a = 5
+    while count_a != -1:
         if count_a == 0:
             print("You've run out of guesses, you lose!")
-            guess_a = False
-        print(f"You have {count_a} attempts remaining to guess the number.")
-        guess = int(input("Make a guess: "))
-        if guess > the_number:
-            print('''Too high.
-Guess again.''')
-        elif guess < the_number:
-            print('''Too low.
-Guess again.''')
         else:
-            print(f"You got it! The answer was {the_number}")
-            guess_a = False
+            print(f"You have {count_a} attempts remaining to guess the number.")
+            guess = int(input("Make a guess: "))
+            if guess > the_number:
+                print("Too high.\nGuess again.")
+            elif guess < the_number:
+                print("Too low.\nGuess again.")
+            else:
+                print(f"You got it! The answer was {the_number}")
+                count_a = 0
         count_a -= 1
 
 def hard_mode():
     count_b = 5
-    guess_b = True
-    while guess_b:
+    while count_b != -1:
         if count_b == 0:
             print("You've run out of guesses, you lose!")
-            guess_b = False
-        print(f"You have {count_b} attempts remaining to guess the number.")
-        guess = int(input("Make a guess: "))
-        if guess > the_number:
-            print('''Too high.
-Guess again.''')
-        elif guess < the_number:
-            print('''Too low.
-Guess again.''')
         else:
-            print(f"You got it! The answer was {the_number}")
-            guess_b = False
+            print(f"You have {count_b} attempts remaining to guess the number.")
+            guess = int(input("Make a guess: "))
+            if guess > the_number:
+                print("Too high.\nGuess again.")
+            elif guess < the_number:
+                print("Too low.\nGuess again.")
+            else:
+                print(f"You got it! The answer was {the_number}")
+                count_b = 0
         count_b -= 1
 
 def difficult_level():
